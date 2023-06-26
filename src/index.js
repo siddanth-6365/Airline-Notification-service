e44286d6aee320ee7ade663263e826415110cd8d
Sidddanth Katipally <siddanth6365@gmail.com>
2023-06-26 22:50:26 +0530

    added amqplib for message queue and consume data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const mailsender = require("./config/email-config")
+const { EmailService} = require('./services')
+const amqplib = require("amqplib");
+
+async function consumequeue() {
+    try {
+        const queue = 'tasks';
+        const connection = await amqplib.connect('amqp://localhost');
+      
+        const ch1 = await connection.createChannel();
+        await ch1.assertQueue(queue);
+    
+        await ch1.consume ( queue ,  (data) => {
+
+          console.log(`${Buffer.from(data.content)}`) ;
+          const object = JSON.parse(`${Buffer.from(data.content)}`);
+         
+           EmailService.sendEmail("serviceairlinenotification@gmail.com", object.recepientEmail, object.subject, object.text);
+          ch1.ack(data); 
+
+        })
+    } catch (error) {
+        console.log(error)
+    }
+     
+    }
+    
 
 const app = express();
 
@@ -13,8 +38,9 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-
-    
-   
+consumequeue()
+console.log("queue is up")
 
 });
+
+
